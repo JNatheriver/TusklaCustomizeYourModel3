fc34dd97a078b1cc1cb60ef48e319c8be26130fa
JNatheriver <jnatheriver@outlook.com>
2024-05-29 00:01:11 -0600

    Converted color section from grid to flex in order to show color balls at center

--- src/color-section.tsx
+++ src/color-section.tsx
@@ -10,25 +10,31 @@ interface ColorSectionProps {
 }
 
 const ColorSection: React.FC<ColorSectionProps> = ({ title, colorSet, selectedColor, handleBodyColorChange, setSelectedColor }) => {
-  const notSelectedColorStyle = 'rounded-full p-2 hover:outline hover:outline-4 hover:outline-offset-[-4px] hover:outline-blue-600 '
-  const selectedColorStyle = 'rounded-full p-2 outline outline-4 outline-offset-[-4px] outline-blue-800'
+  const notSelectedColorStyle = 'rounded-full p-2 max-w-16 hover:outline hover:outline-4 hover:outline-offset-[-4px] hover:outline-blue-600 '
+  const selectedColorStyle = 'rounded-full p-2  max-w-16 outline outline-4 outline-offset-[-4px] outline-blue-800'
 
   return (
-    <section className='grid grid-cols-5 grid-rows-3 px-20'>
-      <h1 className='col-span-5 text-center text-3xl font-semibold font-sans'>{title}</h1>
-      {colorSet.map((color, index) => (
-        <button
-          key={`color ${index}`}
-          className={selectedColor.name === color.name ? selectedColorStyle : notSelectedColorStyle}
-          onClick={() => {
-            handleBodyColorChange(color)
-            setSelectedColor(color)
-          }}
-        >
-          <img src={color.url} alt={color.name} />
-        </button>
-      ))}
-      <p className='col-span-5 text-center pt-3'>
+    <section className='flex flex-col w-full px-20 '>
+      <h1 className='text-center text-3xl font-semibold font-sans'>{title}</h1>
+
+      <menu className='flex flex-row items-center justify-center py-3'>
+        {colorSet.map((color, index) => (
+          <li key={`li-${index}`}>
+            <button
+              key={`color-${index}`}
+              className={selectedColor.name === color.name ? selectedColorStyle : notSelectedColorStyle}
+              onClick={() => {
+                handleBodyColorChange(color)
+                setSelectedColor(color)
+              }}
+            >
+              <img src={color.url} alt={color.name} />
+            </button>
+          </li>
+        ))}
+      </menu>
+
+      <p className='text-center pt-3'>
         <strong>{selectedColor.name}</strong> {selectedColor.price > 0 ? `$${selectedColor.price.toLocaleString()}` : 'Included'}
       </p>
     </section>
